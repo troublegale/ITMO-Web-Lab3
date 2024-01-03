8b3c7bbbd448c54f1f873d583038c7d5f9d626d2
troublegale <eat.some.trouble@gmail.com>
2024-01-03 18:52:36 +0300

    moving

--- src/main/java/web/itmo/lab3_final/model/Point.java
+++ src/main/java/web/itmo/lab3_final/model/Point.java
@@ -1,22 +1,34 @@
-package web.itmo.lab3_final;
+package web.itmo.lab3_final.model;
 
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.io.Serializable;
 
 @Named("point")
 @SessionScoped
+@Entity
+@Table(name = "points")
 public class Point implements Serializable {
 
     public Point() {
         this.r = 1;
     }
 
+    @Id
+    @Column(name = "id")
     private long id;
+    @Column(name = "x")
     private double x;
+    @Column(name = "y")
     private double y;
+    @Column(name = "r")
     private double r;
+    @Column(name = "result")
     private boolean result;
 
     public long getId() {
@@ -58,4 +70,5 @@ public class Point implements Serializable {
     public void setResult(boolean result) {
         this.result = result;
     }
+
 }
